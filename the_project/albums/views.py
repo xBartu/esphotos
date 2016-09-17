d3f253c89d932221fd05cb3bc2dd89a74135eef2
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2016-09-17 20:48:46 +0300

    Albums list view

--- the_project/albums/views.py
+++ the_project/albums/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Album
 
-# Create your views here.
+
+class AlbumList(ListView):
+    model = Album
