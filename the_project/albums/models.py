b42ab7250a1e8cd89bb38ddde0145fbdbebb2250
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2016-09-18 15:25:37 +0300

    Model manager for Photo creation

--- the_project/albums/models.py
+++ the_project/albums/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Album(models.Model):
     """ Album model scheme for the project. Every albun can have many photos
     else it can be happy.
-    Params
+    Args
     name: the name of the album, assumed that the max length for the name is
     25 chars.
     total_photo: the number of the photos in album
@@ -19,23 +19,46 @@ class Album(models.Model):
         return "{}- {}".format(self.name, self.pk)
 
 
+class PhotoManager(models.Manager):
+    """Photo Menager that contains helper methods for
+    Photo model
+    Methods
+    create_photo: a method to create Photo Object and
+    saves it to db
+    """
+    def create_photo(self, org_link, photo, user, album):
+        """ Creating the Photo object and saving to the database
+        Params:
+        org_link: the original URL
+        photo: photo object
+        user: the name of the user who the photo belongs to
+        album: Album object that the photo relates to
+        """
+        photo = self.create(
+            org_link=org_link, photo=photo, user=user, album=album
+        )
+        return photo
+
+
 class Photo(models.Model):
     """The photo model.
-    Params
+    Args
     org_link: the original url of the photo, going to use in
     dublicate detection process. I assume that the link cannot
     be longer than 300 chars.
     photo: The downloaded photo. There is a risk that the photo
     can be removed in the original server. To avoid that, we
     serve the photo in our status
-    user = the user who uploaded the photo to twitter, assumed
+    user: the user who uploaded the photo to twitter, assumed
     that there doesn't need to create a django user for it
-    album = the album which the photo belongs to.
+    album: the album which the photo belongs to.
+    objects: It's the photo create Manager
     """
     org_link = models.URLField(verbose_name='The Original Url', max_length=300)
     photo = models.ImageField(upload_to='albums/photos')
     user = models.CharField(max_length=60)
     album = models.ForeignKey('Album', on_delete=models.CASCADE)
+    objects = PhotoManager()
 
     def __str__(self):
         """Returns
