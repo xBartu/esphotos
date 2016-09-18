8c2240828e1acaa4907de75ae4f25e077ff10c0b
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2016-09-18 20:35:11 +0300

    an url to get 7 popular photos by likes

--- the_project/albums/urls.py
+++ the_project/albums/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import AlbumList
 from .views import AlbumDetail
+from .views import PopularPhotos
 from .views import AlbumViewSet
 from .views import AlbumDetailViewSet
 from .views import SinglePhotoViewSet
@@ -9,6 +10,8 @@ app_name = "albums"
 urlpatterns = [
     url(r'^$', AlbumList.as_view(), name="index"),
     url(r'^(?P<pk>[0-9]+)$', AlbumDetail.as_view(), name='album_detail'),
+    url(r'^popular_photos/(?P<pk>[0-9]+)$',
+        PopularPhotos.as_view(), name='populars'),
     url(r'^api/$', AlbumViewSet.as_view()),
     url(r'^api/album/(?P<album_id>[0-9]+)', AlbumDetailViewSet.as_view()),
     url(r'^api/album/?P<album_id>[0-9]+/photo/?P<pk>[0-9]+',
